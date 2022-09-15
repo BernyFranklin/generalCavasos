b87b76e4336a736c64c9c6c3d00cee507707fb07
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-09-15 14:57:08 -0700

    Basic Maven Shell created

---
+++ src/generalcavasos/src/main/java/com/cis055/App.java
@@ -0,0 +1,18 @@
+package com.cis055;
+
+/*
+ * GeneralCavasos
+ * Frank Bernal
+ * CIS-055
+ * Date Started: 15 Sep 2022
+ * Date Finished: 
+ */
+
+ // Start Class
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
